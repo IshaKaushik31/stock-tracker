b5b687d29050676878700b028cd73fe9776fddb4
Isha Kaushik <ishakaushik3104@gmail.com>
2026-06-29 15:19:05 +0530

    implemented watchlist routes and controller

--- src/controllers/watchlist.controller.js
+++ src/controllers/watchlist.controller.js
@@ -14,4 +14,31 @@ try{
   res.status(500).json({message:error.message});
 }  
 }
-module.exports={add};
+
+async function deleteStock(req,res){
+try{
+  const {id}=req.user;
+  const symbl=req.params.symbol;
+  await pool.query('DELETE FROM watchlist WHERE symbol=$1 AND user_id=$2',[symbl,id]);
+  res.json({message:'stock deleted successfully!!'});
+
+}catch(error){
+  res.status(400).json({message:error.message});
+}  
+  
+
+}
+
+async function seeWatchlist(req,res){
+try{
+  const {id}=req.user;
+  const stocks=await pool.query('SELECT w.symbol, s.curr_price FROM watchlist as w INNER JOIN stocks as s ON s.symbol=w.symbol WHERE w.user_id=$1',[id]);
+  res.json({stocks:stocks.rows})
+}catch(error){
+  res.json({message:error.message});
+}  
+  
+}
+
+
+module.exports={add,deleteStock,seeWatchlist};
--- src/routes/watchlist.js
+++ src/routes/watchlist.js
@@ -1,7 +1,9 @@
-const {add}=require('../controllers/watchlist.controller.js');
+const {add,deleteStock,seeWatchlist}=require('../controllers/watchlist.controller.js');
 const {verifyToken}=require('../middleware/auth.middleware.js');
 const express=require('express');
 const router=express.Router();
 
 router.post('/',verifyToken,add);
+router.delete('/:symbol',verifyToken,deleteStock);
+router.get('/',verifyToken,seeWatchlist);
 module.exports=router;
